0a854bcf59b2e7ee5bc183d2629de824b295b25f
Ambika Muscarello <ammuscarello@gmail.com>
2018-05-22 22:37:47 -0400

    change destroy route

--- app.rb
+++ app.rb
@@ -64,10 +64,12 @@ get '/users' do
     # erb :edit
 end
 
+# renders edit user id
 get '/edit' do
     @this_user = User.find(session[:id])
     erb :edit
 end
+
 
 put '/user/user_id' do
     @this_user = User.find(session[:id])
@@ -77,62 +79,68 @@ put '/user/user_id' do
     @this_user.update(birthday: x, email: y, password: z)
     redirect '/profile'
 end
-
+#WORKS  takes you to delete page
 get '/delete' do
     erb :delete
 end
-
+#  WORKS deletes user
 delete '/user/:id' do
-    User.destroy(params[:id])
+    @this_user = User.find(params[:id])
+    @this_user.destroy
     redirect '/user'
 end
-
+# renders post
 get '/post' do
+    @this_post = Post.where(user_id: session[:id])
     erb :post
 end
 
-# post '/post/create/new' do
-#     @posts = Post.where(user_id: session[:id])
-#     @posts = Post.limit(20)
-#     Post.create(post_name: params[:post_name], post_content: params[:post_content], user_id:session[:id])
-#     redirect '/profile'
-# end
+get '/post/user_id' do
+    @this_user = User.find(params[:id])
+    erb :post
+end
 
 get '/new_post' do
     erb :new_post
+end
+
+post '/post/create/new' do
+    @posts = Post.where(user_id: session[:id])
+    @posts = Post.limit(20)
+    Post.create(post_name: params[:post_name], post_content: params[:post_content], user_id:session[:id])
+    redirect '/profile'
 end
 
 post '/post/submit' do
     Post.create(params[:user_id])
     redirect '/post'
 end
-
-put '/post/user_id' do
-    @this_user = User.find(session[:id])
-    a = (post_name: params[:post_name], post_content: params[:post_content])
-    @this_user.update(post_name: a, post_content: a)
+#  starts to break here >>>>>
+# allow to edit posts
+put '/post/:id' do
+    @post = Post.find(params[:id])
+    @post.update(post_name: params[:post_name], post_content: params[:post_content], user_id: session[:id])
     redirect '/post'
 end
 
-
-
-
-
-
-get '/post/:user_id' do
-    @posts = Post.find(params[:user_id])
-    @posts = Post.order("created_at DESC")
+# get '/post/user_id/edit' do
+#     @posts = Post.find(params[:user_id])
+#     @posts = Post.order("created_at DESC")
     # @posts = Post.order('created_at: :desc').limit(20)
-    @categories = Category.order(post: :desc)
-    erb :post
-end
+    # @categories = Category.order(post: :desc)
+#     erb :post
+# end
 
 
 #to edit the post
-get '/post/:user_id' do
-    @posts = Post.find(params[:user_id])
-    erb :post
-end
+# put '/post/user_id/edit' do
+    # @posts = Post.find(session[:id])
+#     @this_user = User.find(session[:id])
+#     a = params[:post_name]
+#     b = params[:post_content]
+#     @this_user.update(post_name: a, post_content: b)
+#     redirect '/post'
+# end
 
 get '/logout' do
     erb :logout
